c3ad2a14f6e726b75859187e14a02e957b9d80cf
restjun <150533379+restjun@users.noreply.github.com>
2025-08-08 13:02:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -285,7 +285,7 @@ def main():
             continue
 
         df_1h = get_ohlcv_okx(inst_id, bar='1H', limit=60)
-        if df_1h is None or not is_recent_20_50_golden_cross_and_still_bullish(df_1h, limit=10):
+        if df_1h is None or not is_recent_20_50_golden_cross_and_still_bullish(df_1h, limit=15):
             continue
 
         daily_change = calculate_daily_change(inst_id)
