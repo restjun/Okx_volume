313a3b1862a2980779068f0ea74ace9be1715c8e
restjun <150533379+restjun@users.noreply.github.com>
2025-08-16 16:53:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ def send_ranked_volume_message(top_bullish, total_count, bullish_count, volume_r
         inst_id = item[0]
         volume_1h = dict(all_volume_data).get(inst_id, 0)
         rank = volume_rank_map.get(inst_id)
-        if volume_1h < 1_000_000 or rank is None or rank > 10:
+        if volume_1h < 1_000_000 or rank is None or rank > 20:
             continue
         filtered_top_bullish.append((inst_id, item[1], item[2], volume_1h, rank))
 
